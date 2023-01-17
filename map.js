a4a001630c21135d304688f5d86eb196c98956c8
Kenny <zhangkune24@gmail.com>
2023-01-17 14:36:01 +0800

    Map

--- map.js
+++ map.js
@@ -5,5 +5,5 @@ m.get(o); // content
 m.has(o); // true
 m.delete(o); // true
 m.has(o); // false
-//WeakMap 的 key 只能是 Object 类型，弱引用键（一个对象的其他引用全部消失
-//则weakmap的key会被垃圾回收，同时对应的value也会被清除），无法枚举
+// WeakMap 的 key 只能是 Object 类型，弱引用键（一个对象的其他引用全部消失
+// 则weakmap的key会被垃圾回收，同时对应的value也会被清除），无法枚举
