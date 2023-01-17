3240e2a650c3a7e69825abe2c2488ac7aac73e84
Kenny <zhangkune24@gmail.com>
2023-01-17 14:38:01 +0800

    Object.create

--- object_create.js
+++ object_create.js
@@ -1,14 +1,14 @@
-//Object.create()
+// Object.create()
 let a = Object.create(
   {
-    x: 123, //定义在原型对象上
+    x: 123, // 定义在原型对象上
   },
   {
     like: {
       value: "fish", // 初始化赋值
       writable: true, // 是否是可改写的
       configurable: true, // 是否能够删除，是否能够被修改
-      enumerable: true, //是否可以用for in 进行枚举
+      enumerable: true, // 是否可以用for in 进行枚举
     },
   }
 );
