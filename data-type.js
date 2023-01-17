cce1b896ca285efeb80a2228d3da000fe7b896c6
Kenny <zhangkune24@gmail.com>
2023-01-17 14:30:59 +0800

    判断数据类型

--- data-type.js
+++ data-type.js
@@ -1,11 +1,11 @@
-//typeof
-typeof ""; //string
-//instanceof
-[] instanceof Array; //true
-//isPrototypeOf
+// typeof
+typeof ""; // string
+// instanceof
+[] instanceof Array; // true
+// isPrototypeOf
 function Foo() {}
-Foo.prototype.isPrototypeOf(new Foo()); //true
-//constructor
-"".constructor === String; //true
-//Object.prototype.toString.call
-Object.prototype.toString.call(""); //[Object String]
+Foo.prototype.isPrototypeOf(new Foo()); // true
+// constructor
+"".constructor === String; // true
+// Object.prototype.toString.call
+Object.prototype.toString.call(""); // [Object String]
