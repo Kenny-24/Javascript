663ffa2c0331fb10a199d082936e7c5b834718ee
Kenny <zhangkune24@gmail.com>
2023-01-17 14:43:01 +0800

    Reflect

--- reflect.js
+++ reflect.js
@@ -1,16 +1,16 @@
 // Reflect.apply
-var obj1 = {};
+const obj1 = {};
 Reflect.apply(Math.floor, obj1, [1.88]); // 1;
 
 // Reflect.construct
 const obj2 = Reflect.construct(Date, [2021, 3, 1]);
 
 // Reflect.get
-var obj3 = { x: 1, y: 2 };
+const obj3 = { x: 1, y: 2 };
 Reflect.get(obj3, "x"); // 1
 
 // Reflect.set
-var obj4 = {};
+const obj4 = {};
 Reflect.set(obj4, "prop", "value"); // true
 
 // Reflect.defineProperty(
